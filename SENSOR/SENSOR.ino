4c67a1d523ca2d1c3944288429d40fc306c8b6b1
Josetrujillo2021 <tru19452@uvg.edu.gt>
2021-10-29 21:33:04 -0600

    documentacion y creacion de funcion
    En este commit se realizó la documentación de la programación en la TIVA C y se realizó la función que controlará las instrucciones para el sensor HC-SR04

--- SENSOR/SENSOR.ino
+++ SENSOR/SENSOR.ino
@@ -1,41 +1,89 @@
+//Universidad Del Valle de Guatemala
+//BE3015: Electrónica Digital 2
+//José Trujillo
+//Proyecto #2
+//------------------------------------------------------------------------------------------------
+
+//----------------------------------------------------------------------------------------------------------------------
+//Librerías
+//----------------------------------------------------------------------------------------------------------------------
 #include <Arduino.h>
-#define Trigger PE_4
-#define Echo PD_2
-//const int Trigger = 12;   //Pin digital 2 para el Trigger del sensor
-//const int Echo = 13;   //Pin digital 3 para el Echo del sensor
-long InicialTime; 
+
+//----------------------------------------------------------------------------------------------------------------------
+//Definición de pines
+//----------------------------------------------------------------------------------------------------------------------
+const int Trigger = 12;   //Pin digital 2 para el Trigger del sensor
+const int Echo = 13;   //Pin digital 3 para el Echo del sensor
+
+//----------------------------------------------------------------------------------------------------------------------
+//Prototipos de funciones
+//----------------------------------------------------------------------------------------------------------------------
+void sensorProximidad(void);
+//---------------------------------------------------------------------------------------------------------------------
+//Variables Globales
+//----------------------------------------------------------------------------------------------------------------------
+
+//Son de tipo long debido a que su dato es en la medición de tiempo
+long t; //timepo que demora en llegar el eco
+long d; //distancia en centimetros
+
+//DELAY 1 = 10 microsegundos
+long LastTime1;
+long tiempo1 = 10;  
+
+//DELAY 2 = 1000 milisegundos
+long LastTime2; 
+long tiempo2 = 1000; 
+//----------------------------------------------------------------------------------------------------------------------
+//ISR  (interrupciones)
+//----------------------------------------------------------------------------------------------------------------------
+
+//----------------------------------------------------------------------------------------------------------------------
+//CONFIGURACIÓN
+//----------------------------------------------------------------------------------------------------------------------
 void setup() {
   Serial.begin(115200);//iniciailzamos la comunicación
   pinMode(Trigger, OUTPUT); //pin como salida
   pinMode(Echo, INPUT);  //pin como entrada
   digitalWrite(Trigger, LOW);//Inicializamos el pin con 0
-  InicialTime=micros(); 
+
+  //tiempos
+  LastTime1=micros(); 
+  LastTime2=millis();
 }
 
+
+//---------------------------------------------------------------------------------------------------------------------
+//Loop principal
+//---------------------------------------------------------------------------------------------------------------------
 void loop()
 {
-  long t; //timepo que demora en llegar el eco
-  long d; //distancia en centimetros
-  long t2; 
-  long d2;
+  if(millis()-LastTime2 >= tiempo2){
+    sensorProximidad();   
+    LastTime2 = millis(); 
+  }
+}
+
+//---------------------------------------------------------------------------------------------------------------------
+//Sensor proximidad
+//---------------------------------------------------------------------------------------------------------------------
+void sensorProximidad(void){
+  // este if permite mandar la señal de disparo del sensor con un ancho de banda de 10us en alto, luego se apaga
+  if (micros()- LastTime1 >= tiempo1){
+    digitalWrite(Trigger, HIGH);  
+    LastTime1 =micros(); 
+  }
   
-  int V1 = 0; 
-  
-  
-  digitalWrite(Trigger, HIGH);
-  delayMicroseconds(10);          //Enviamos un pulso de 10us
   digitalWrite(Trigger, LOW);
 
-  t = pulseIn(Echo, HIGH); //obtenemos el ancho del pulso //preguntar si puedo usar transistores para poder comunicar al SENSOR con el OPAM. el triger va conectado a 5V y el transitor  del pin detrigger del 
-  //procesador va a 3.3V luego la del transistor del echo del sensor ba a la base y 3.3v a el procesador
-  d = t/59;             //escalamos el tiempo a una distancia en cm
+  //obtenemos el ancho del pulso 
+  t = pulseIn(Echo, HIGH);
+  //escalamos el tiempo a una distancia en cm 
+  d = t/59;             
 
-  
+  //Enviamos serialmente el valor de la distancia
   Serial.print("Distancia: ");
-  Serial.print(d);      //Enviamos serialmente el valor de la distancia
+  Serial.print(d);      
   Serial.print("cm");
   Serial.println();
-  Serial.println(t);
-  
-  delay(1000);      //Hacemos una pausa de 100ms
 }
