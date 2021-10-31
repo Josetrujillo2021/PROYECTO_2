b2293a93c4589484ac8473a31385d39fffdb54d7
Josetrujillo2021 <tru19452@uvg.edu.gt>
2021-10-30 18:27:12 -0600

    Se generaron notas musicales pinout
    Se generaron los docs de notas musciales y pinout para poder generar sonido con las alarmas

--- notas_musicales/notas_musicales.ino
+++ notas_musicales/notas_musicales.ino
@@ -1,5 +1,5 @@
 
-
+#include <Arduino.h>
 #define NOTE_B0  31
 
 #define NOTE_C1  33
@@ -181,34 +181,56 @@
 
 int melody[] = {
 
-   NOTE_C4, NOTE_G3, NOTE_G3, NOTE_A3, NOTE_G3, 0, NOTE_B3, NOTE_C4
+   NOTE_F7, NOTE_F6
 
+};
+
+int medicion[] = {
+  NOTE_B0, NOTE_A7
 };
 
 
 // note durations: 4 = quarter note, 8 = eighth note, etc.:
-
+ 
 int noteDurations[] = {
-   4, 8, 8, 4, 4, 4, 4, 4
+   4, 16
+};
+
+int noteDurations1[] = {
+   8, 8
 };
 
 void setup() {
-  // iterate over the notes of the melody:
-  for (int thisNote = 0; thisNote < 8; thisNote++) {
+
+ for (int thisNote = 0; thisNote < 2; thisNote++) {
+      // to calculate the note duration, take one second divided by the note type.
+      //e.g. quarter note = 1000 / 4, eighth note = 1000/8, etc.
+      int noteDuration1 = 1000 / noteDurations1[thisNote];
+      tone(PF_2, medicion[thisNote], noteDuration1);
+      // to distinguish the notes, set a minimum time between them.
+      // the note's duration + 30% seems to work well:
+      int pauseBetweenNotes = noteDuration1 * 1.30;
+      delay(pauseBetweenNotes);
+      // stop the tone playing:
+      noTone(PF_2);
+  }
+
+   /*for (int thisNote = 0; thisNote < 2; thisNote++) {
       // to calculate the note duration, take one second divided by the note type.
       //e.g. quarter note = 1000 / 4, eighth note = 1000/8, etc.
       int noteDuration = 1000 / noteDurations[thisNote];
-      tone(8, melody[thisNote], noteDuration);
+      tone(PF_2, melody[thisNote], noteDuration);
       // to distinguish the notes, set a minimum time between them.
       // the note's duration + 30% seems to work well:
       int pauseBetweenNotes = noteDuration * 1.30;
       delay(pauseBetweenNotes);
       // stop the tone playing:
-      noTone(8);
-  }
+      noTone(PF_2);
+  }*/
 
 }
 
 void loop() {
-   // no need to repeat the melody.
+
+    
 }
