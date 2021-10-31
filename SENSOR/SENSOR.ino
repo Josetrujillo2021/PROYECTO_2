77292a94480e289a56a306de762b017cfd474b3f
Josetrujillo2021 <tru19452@uvg.edu.gt>
2021-10-31 08:49:12 -0600

    NOTAS DE BUZZER
    Se agregaron las instrucciones que haran sonar al buzzer cada vez que se guarde una nota y cada vez que se tome un dato

--- SENSOR/SENSOR.ino
+++ SENSOR/SENSOR.ino
@@ -16,13 +16,21 @@
 #define B1 PF_0 
 #define B2 PF_4
 
-const int Trigger = PE_4;   //Pin digital 2 para el Trigger del sensor
-const int Echo = PD_2;   //Pin digital 3 para el Echo del sensor
+#define Trigger PE_4   //Pin digital 2 para el Trigger del sensor
+#define Echo PD_2   //Pin digital 3 para el Echo del sensor
 
+#define buzzer PF_2
+
+//notas 
+#define NOTE_F7  2794
+#define NOTE_F6  1397
+#define NOTE_B0  31
+#define NOTE_A7  3520
 //----------------------------------------------------------------------------------------------------------------------
 //Prototipos de funciones
 //----------------------------------------------------------------------------------------------------------------------
 void sensorProximidad(void);
+void guardarDatoSD(void);
 //---------------------------------------------------------------------------------------------------------------------
 //Variables Globales
 //----------------------------------------------------------------------------------------------------------------------
@@ -37,6 +45,15 @@ long LastTime1;
 //DELAY 2 = 1000 milisegundos
 long LastTime2; 
 long tiempo2 = 1000; 
+
+//Melodias del buzzer
+int melody[] = { NOTE_F7, NOTE_F6 };
+int medicion[] = { NOTE_B0, NOTE_A7 };
+
+//Duración de las notas
+int noteDurations[] = {4, 16};
+int noteDurations1[] = {8, 8};
+
 //----------------------------------------------------------------------------------------------------------------------
 //ISR  (interrupciones)
 //----------------------------------------------------------------------------------------------------------------------
@@ -66,10 +83,8 @@ void setup() {
 //---------------------------------------------------------------------------------------------------------------------
 void loop()
 {
-  //if(millis()-LastTime2 >= tiempo2){
-    sensorProximidad();   
-    //LastTime2 = millis(); 
-  //}
+  sensorProximidad();   
+  guardarDatoSD(); 
 }
 
 //---------------------------------------------------------------------------------------------------------------------
@@ -89,6 +104,17 @@ void sensorProximidad(void){
 
   //Enviamos serialmente el valor de la distancia
   if (digitalRead(B1)==0){
+    //Alarma Buzzer
+    for (int thisNote = 0; thisNote < 2; thisNote++) {
+      //Tiempo de la nota
+      int noteDuration1 = 1000 / noteDurations1[thisNote];
+      tone(buzzer, medicion[thisNote], noteDuration1);
+      //Tiempo entre nota y nota
+      int pauseBetweenNotes = noteDuration1 * 1.30;
+      delay(pauseBetweenNotes);
+      //detiene la nota que esta sonando
+      noTone(buzzer);
+    }
     delay(10);
     if(digitalRead(B1)==1){
       Serial.print("Distancia: ");
@@ -96,7 +122,27 @@ void sensorProximidad(void){
       Serial.print("cm");
       Serial.println();  
    }
-  }
-  
-  
+  } 
+}
+
+//---------------------------------------------------------------------------------------------------------------------
+//Guardar datos en la SD
+//---------------------------------------------------------------------------------------------------------------------
+void guadarDatoSD(void){
+ if (digitalRead(B2)==0){
+   for (int thisNote = 0; thisNote < 2; thisNote++) {
+      //Alarma Buzzer
+      int noteDuration = 1000 / noteDurations[thisNote];
+      tone(buzzer, melody[thisNote], noteDuration);
+      //iempo entre nota y nota
+      int pauseBetweenNotes = noteDuration * 1.30;
+      delay(pauseBetweenNotes);
+      //se detiene la nota que esta sonando
+      noTone(buzzer);
+    }
+   delay(10);
+    if(digitalRead(B2)==1){
+      //aqui debo de agragar la función que se comunicara con ESP32 para guardar el dato
+    }
+}
 }
