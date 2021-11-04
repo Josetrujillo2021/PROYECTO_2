405be64540cadfcc6b3ba412dd28ae9adc388e2d
Josetrujillo2021 <tru19452@uvg.edu.gt>
2021-11-04 16:52:43 -0600

    Sensor en ESP32
    Se actualizo la programación para que el ESP32 sea el que controle el sensor HC-SRO4 y se comunique con la TIVAC

--- PROYECTO 2 ESP32/src/main.cpp
+++ PROYECTO 2 ESP32/src/main.cpp
@@ -1,62 +1,114 @@
+//Universidad Del Valle de Guatemala
+//BE3015: Electrónica Digital 2
+//José Trujillo
+//Proyecto #2
+//------------------------------------------------------------------------------------------------
 
+//----------------------------------------------------------------------------------------------------------------------
+//Librerías
+//----------------------------------------------------------------------------------------------------------------------
 #include <Arduino.h>
-#include <SPI.h>
-#include <SD.h>
-#define cs 5
-File archivo;
 
+//----------------------------------------------------------------------------------------------------------------------
+//Definición de pines
+//----------------------------------------------------------------------------------------------------------------------
+
+
+#define Trigger 32   //Pin digital 2 para el Trigger del sensor
+#define Echo 14   //Pin digital 3 para el Echo del sensor
+
+//----------------------------------------------------------------------------------------------------------------------
+//Prototipos de funciones
+//----------------------------------------------------------------------------------------------------------------------
+void sensorProximidad(void);
+void guardarDatoSD(void);
+//---------------------------------------------------------------------------------------------------------------------
+//Variables Globales
+//----------------------------------------------------------------------------------------------------------------------
+
+//Son de tipo long debido a que su dato es en la medición de tiempo
+long t; //timepo que demora en llegar el eco
+long d; //distancia en centimetros
+
+//DELAY 1 = 10 microsegundos
+long LastTime1;
+
+//DELAY 2 = 1000 milisegundos
+long LastTime2; 
+long tiempo2 = 1000; 
+
+String medidor = ""; 
+String guardado= ""; 
+//----------------------------------------------------------------------------------------------------------------------
+//ISR  (interrupciones)
+//----------------------------------------------------------------------------------------------------------------------
+
+//----------------------------------------------------------------------------------------------------------------------
+//CONFIGURACIÓN
+//----------------------------------------------------------------------------------------------------------------------
 void setup() {
-  // Open serial communications and wait for port to open:
-  Serial.begin(115200);
-  while (!Serial) {
-    ; // wait for serial port to connect. Needed for native USB port only
-  }
+  Serial.begin(115200);//iniciailzamos la comunicación
+  Serial1.begin(115200);
+  pinMode(Trigger, OUTPUT); //pin como salida
+  pinMode(Echo, INPUT);  //pin como entrada
+  
+  digitalWrite(Trigger, LOW);//Inicializamos el pin con 0
 
-
-  Serial.print("Initializing SD card...");
-
-  pinMode(cs, OUTPUT);
-  //SPI.(0); 
-  //Se esta inicializando la tarjeta SD
-  if (!SD.begin(cs)) {
-    Serial.println("initialization failed!");
-    while (1);
-  }
-  Serial.println("initialization done.");
-
-  //Parametros: 1:archivo a utilizar 2: es para ver si es para escritura o lectura
-  //Si es lectura  se puede omitir
-  archivo = SD.open("datos.txt", FILE_WRITE);
-
-  // if the file opened okay, write to it:
-  if (archivo) {
-    Serial.print("Writing to test.txt...");
-    archivo.println("testing 1, 2, 3.");
-    // Siempre cerrar el archivo cuando se modifique
-    archivo.close();
-    Serial.println("done.");
-  } else {
-    // if the file didn't open, print an error:
-    Serial.println("error opening test.txt");
-  }
-
-  // re-open the file for reading:
-  archivo = SD.open("test.txt");
-  if (archivo) {
-    Serial.println("test.txt:");
-
-    // read from the file until there's nothing else in it:
-    while (archivo.available()) {
-      Serial.write(archivo.read());
-    }
-    // close the file:
-    archivo.close();
-  } else {
-    // if the file didn't open, print an error:
-    Serial.println("error opening test.txt");
-  }
+  //tiempos
+  LastTime1=micros(); 
+  LastTime2=millis();
 }
 
-void loop() {
-  // nothing happens after setup
+
+//---------------------------------------------------------------------------------------------------------------------
+//Loop principal
+//---------------------------------------------------------------------------------------------------------------------
+void loop()
+{
+  sensorProximidad();   
+  guardarDatoSD(); 
+}
+
+//---------------------------------------------------------------------------------------------------------------------
+//Sensor proximidad
+//---------------------------------------------------------------------------------------------------------------------
+void sensorProximidad(void){
+  // este while permite mandar la señal de disparo del sensor con un ancho de banda de 10us en alto, luego se apaga
+  digitalWrite(Trigger, HIGH); 
+  LastTime1=micros();  
+  while(micros()<LastTime1+10) ; 
+  digitalWrite(Trigger, LOW);
+
+  //obtenemos el ancho del pulso 
+  t = pulseIn(Echo, HIGH);
+  //escalamos el tiempo a una distancia en cm 
+  d = t/59;             
+
+  //permite leer lo que se mande al canal serial 1 hasta que exista un espacio y lo guarda en la variable 
+  medidor = Serial1.readStringUntil('\n');
+
+  //Enviamos serialmente el valor de la distancia
+  if (medidor = 'medir'){
+      //Comunicación con monitor
+      Serial.print("Distancia: ");
+      Serial.print(d);      
+      Serial.print("cm");
+      Serial.println();  
+      //Comunicación con TIVA
+      Serial1.println(d);
+  } 
+}
+
+//---------------------------------------------------------------------------------------------------------------------
+//Guardar datos en la SD
+//---------------------------------------------------------------------------------------------------------------------
+void guardarDatoSD(void){
+  //permite leer lo que se mande al canal serial 1 hasta que exista un espacio y lo guarda en la variable 
+  guardado= Serial1.readStringUntil('\n');
+  if (guardado = 'guardar'){
+    //Comunicacion con UART
+    Serial.println("Se guardaron los datos en la memoria SD"); 
+    //Se envia el dato a TIVA para que lo guarde
+    Serial1.println(d);
+  }
 }
