e88e4b8a37db0491cdf179f04346d2537b786435
Josetrujillo2021 <tru19452@uvg.edu.gt>
2021-11-04 18:19:49 -0600

    Progra TFT
    Se añadio la programación de la TFT para hacerla funcionar de una vez con el sensor

--- SENSOR/SENSOR.ino
+++ SENSOR/SENSOR.ino
@@ -9,6 +9,25 @@
 //----------------------------------------------------------------------------------------------------------------------
 #include <Arduino.h>
 
+//librerías para TFT
+#include <stdint.h>
+#include <stdbool.h>
+#include <TM4C123GH6PM.h>
+#include "inc/hw_ints.h"
+#include "inc/hw_memmap.h"
+#include "inc/hw_types.h"
+#include "driverlib/debug.h"
+#include "driverlib/gpio.h"
+#include "driverlib/interrupt.h"
+#include "driverlib/rom_map.h"
+#include "driverlib/rom.h"
+#include "driverlib/sysctl.h"
+#include "driverlib/timer.h"
+
+#include "bitmaps.h"
+#include "font.h"
+#include "lcd_registers.h"
+
 //----------------------------------------------------------------------------------------------------------------------
 //Definición de pines
 //----------------------------------------------------------------------------------------------------------------------
@@ -16,8 +35,6 @@
 #define Boton1 PF_0 
 #define Boton2 PF_4
 
-//#define Trigger PE_4   //Pin digital 2 para el Trigger del sensor
-//#define Echo PD_2   //Pin digital 3 para el Echo del sensor
 
 #define buzzer PF_2
 
@@ -26,19 +43,42 @@
 #define NOTE_F6  1397
 #define NOTE_B0  31
 #define NOTE_A7  3520
+
+//Pines para TFT 
+#define LCD_RST PD_0
+#define LCD_CS PD_1
+#define LCD_RS PD_2
+#define LCD_WR PD_3
+#define LCD_RD PE_1
+int DPINS[] = {PB_0, PB_1, PB_2, PB_3, PB_4, PB_5, PB_6, PB_7};
 //----------------------------------------------------------------------------------------------------------------------
 //Prototipos de funciones
 //----------------------------------------------------------------------------------------------------------------------
 void sensorProximidad(void);
 void guardarDatoSD(void);
 void lecturaDatos(void);
+
+//funciones para TFT
+void LCD_Init(void);
+void LCD_CMD(uint8_t cmd);
+void LCD_DATA(uint8_t data);
+void SetWindows(unsigned int x1, unsigned int y1, unsigned int x2, unsigned int y2);
+void LCD_Clear(unsigned int c);
+void H_line(unsigned int x, unsigned int y, unsigned int l, unsigned int c);
+void V_line(unsigned int x, unsigned int y, unsigned int l, unsigned int c);
+void Rect(unsigned int x, unsigned int y, unsigned int w, unsigned int h, unsigned int c);
+void FillRect(unsigned int x, unsigned int y, unsigned int w, unsigned int h, unsigned int c);
+void LCD_Print(String text, int x, int y, int fontSize, int color, int background);
+
+void LCD_Bitmap(unsigned int x, unsigned int y, unsigned int width, unsigned int height, unsigned char bitmap[]);
+void LCD_Sprite(int x, int y, int width, int height, unsigned char bitmap[], int columns, int index, char flip, char offset);
+
+
 //---------------------------------------------------------------------------------------------------------------------
 //Variables Globales
 //----------------------------------------------------------------------------------------------------------------------
-
-//Son de tipo long debido a que su dato es en la medición de tiempo
-//long t; //timepo que demora en llegar el eco
-//long d; //distancia en centimetros
+//Variable para TFT 
+extern uint8_t fondo[];
 
 //DELAY 1 = 10 microsegundos
 long LastTime1;
@@ -79,6 +119,19 @@ void setup() {
   //tiempos
   LastTime1=micros(); 
   LastTime2=millis();
+
+  //Inicializacion de TFT
+  SysCtlClockSet(SYSCTL_SYSDIV_2_5 | SYSCTL_USE_PLL | SYSCTL_OSC_MAIN | SYSCTL_XTAL_16MHZ);
+  GPIOPadConfigSet(GPIO_PORTB_BASE, 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7, GPIO_STRENGTH_8MA, GPIO_PIN_TYPE_STD_WPU);
+
+  LCD_Init();
+  LCD_Clear(0x00);
+  FillRect(0, 0, 319, 239, 0xFFFF);
+  FillRect(50, 60, 20, 20, 0xF800);
+  FillRect(70, 60, 20, 20, 0x07E0);
+  FillRect(90, 60, 20, 20, 0x001F);
+
+  String text1 = "Sensor de proximidad";
 }
 
 
@@ -87,6 +140,11 @@ void setup() {
 //---------------------------------------------------------------------------------------------------------------------
 void loop()
 {
+   for (int x = 0; x < 320 - 32; x++) {
+    int anim2 = (x / 35) % 4;
+    LCD_Sprite(60, 100, 32, 32, pesaSprite, 4, anim2, 0, 1);
+    delay(15);
+  }
   sensorProximidad();   
   guardarDatoSD(); 
   lecturaDatos();
